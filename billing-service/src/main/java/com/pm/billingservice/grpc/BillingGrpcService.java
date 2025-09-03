2e517e63814f368271da863cec58168cd863aa39
jiteshjitsun <jiteshjitsun@gmail.com>
2025-09-03 14:07:42 +0530

    fix: grpc wasn't generating wrong dir

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,10 @@
+package com.pm.billingservice.grpc;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    // Implement your gRPC service methods here
+
+}
